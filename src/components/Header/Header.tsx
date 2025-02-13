8faabddfd0cfed45f84bc157eec451fc1c332710
Uday kumar Gangapuram <gangapuramudaykumar@gmail.com>
2025-02-13 23:17:56 +0530

    shadcn and routes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
 const Header = () => {
   return (
     <div>
-        
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+        </MenubarMenu>
+      </Menubar>
     </div>
   )
 }
